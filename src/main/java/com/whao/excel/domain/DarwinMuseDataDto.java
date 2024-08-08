54a527dab9a5851d2f1a712e28c5bd42fa81219d
xiongwh <1305257824@qq.com>
2024-08-09 00:54:48 +0800

    add

---
+++ src/main/java/com/whao/excel/domain/DarwinMuseDataDto.java
@@ -0,0 +1,29 @@
+package com.whao.excel.domain;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author xiongwh
+ * @date 2024/8/9 12:21 AM
+ */
+@Data
+public class DarwinMuseDataDto {
+
+    @ExcelProperty("trace_id")
+    private String traceId;
+
+    @ExcelProperty("darwin_data")
+    private String darwinData;
+
+    @ExcelProperty("muse_data")
+    private String museData;
+
+    @ExcelProperty("darwin_time")
+    private Date darwinDate;
+
+    @ExcelProperty("muse_time")
+    private Date museDate;
+}
