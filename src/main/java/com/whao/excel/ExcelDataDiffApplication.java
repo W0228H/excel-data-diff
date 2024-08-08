0dab278711cbeb00e58a1f33c57502e0599219f3
xiongwh <1305257824@qq.com>
2024-08-09 00:20:54 +0800

    Initial commit

---
+++ src/main/java/com/whao/excel/ExcelDataDiffApplication.java
@@ -0,0 +1,16 @@
+package com.whao.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiongwh
+ * @date 2024/8/8 11:26 PM
+ */
+
+@SpringBootApplication
+public class ExcelDataDiffApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelDataDiffApplication.class, args);
+    }
+}
